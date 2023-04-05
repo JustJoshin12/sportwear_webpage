85ed5fa5de814d9a157fa46dd127b2136a576c35
Joshua Smith <kazenostigma50@gmail.com>
2023-04-05 13:50:41 -0500

    Added a nav home new and colection selection

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+// SWIPER PRODUCTS
+
+let swiperProducts = new Swiper(".products__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    1024: {
+      spaceBetween: 72,
+    },
+  },
+});
